bef6eaab5e6634126a8062bc8042235e7a00a077
Imanol-Ortega <imanicoortemohr018@gmail.com>
2024-12-09 07:14:23 -0300

    probando

--- Minecraft-Status/src/services/ApiService.ts
+++ Minecraft-Status/src/services/ApiService.ts
@@ -1,6 +1,6 @@
 import type { APIMC } from "@/types/interfaces";
 import axios from "axios"
 export async function getStatus(): Promise<APIMC>{
-    const response = await axios.get<APIMC>(`https://api.mcstatus.io/v2/status/java/${import.meta.env.PUBLIC_IP_SV_MC}`);
+    const response = await axios.get<APIMC>(`https://api.mcstatus.io/v2/status/java/181.122.92.245`);
     return response.data;
 }
